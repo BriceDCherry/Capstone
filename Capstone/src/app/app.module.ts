aefa35bcbfdfec5e6124c377479cc26aef2cff61
bcherry4 <bcherry4@ford.com>
2022-06-09 11:14:30 -0600

    Add componenets html and css

--- Capstone/src/app/app.module.ts
+++ Capstone/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapstoneComponent } from './capstone/capstone.component';
+import { HouseComponent } from './house/house.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapstoneComponent,
+    HouseComponent
   ],
   imports: [
     BrowserModule,
